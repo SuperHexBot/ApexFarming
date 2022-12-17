a5bc79719ae617bb41b2caa6f152f4eee5631889
SuperHexBot <65476506+SuperHexBot@users.noreply.github.com>
2022-12-17 23:05:01 +0800

    1
    1

--- ApexFarmer.py
+++ ApexFarmer.py
@@ -7,11 +7,16 @@ import random
 Random = ['a','w','s','d','4','q','1','2','3','4']
 
 while True:
-    if pyautogui.locateOnScreen('ManuNotReady.png', region=(0,538,447,528), grayscale=True, confidence=0.7) != None:
+    if pyautogui.locateOnScreen('retry.png', region=(773,581,379,304), grayscale=True, confidence=0.6) != None:
+         pyautogui.click(952, 717)
+         time.sleep(0.5)
+         pyautogui.click(952, 717)
+     
+    if pyautogui.locateOnScreen('ManuNotReady.png', region=(0,538,447,528), grayscale=True, confidence=0.6) != None:
         pyautogui.click(230, 950)
         time.sleep(0.5)
      
-    if pyautogui.locateOnScreen('ManuNotReady2.png', region=(0,538,447,528), grayscale=True, confidence=0.7) != None:
+    if pyautogui.locateOnScreen('ManuNotReady2.png', region=(0,538,447,528), grayscale=True, confidence=0.6) != None:
         pyautogui.click(230, 950)
         time.sleep(0.5)
         
@@ -25,18 +30,23 @@ while True:
          time.sleep(5)
     
     
-    if pyautogui.locateOnScreen('InGame.png', region=(87,755,379,304), grayscale=True, confidence=0.5) != None:
+    if pyautogui.locateOnScreen('InGame.png', region=(87,755,379,304), grayscale=True, confidence=0.3) != None:
          print("In game waiting")
          keyboard.press_and_release(Random)
          time.sleep(0.5)
     
     if pyautogui.locateOnScreen('dead.png', region=(441,19,1017,304), grayscale=True, confidence=0.6) != None:
-                pyautogui.click(1771, 1040)
-                time.sleep(0.5)
-                pyautogui.click(1771, 1040)
+           print("dead")
+           pyautogui.click(1771, 1040)
+           time.sleep(0.5)
+           pyautogui.click(1771, 1040)
+           time.sleep(0.5)
+           keyboard.press_and_release('space')
+           time.sleep(1.0)
                 
     
     if pyautogui.locateOnScreen('leave.png', region=(0,0,1920,1080), grayscale=True, confidence=0.6) != None:
+                print("leave")
                 pyautogui.click(963, 623)
                 time.sleep(0.5)
      
@@ -49,15 +59,9 @@ while True:
          pyautogui.click(850, 713)
          
     if pyautogui.locateOnScreen('jump.PNG', region=(715,743,513,304), grayscale=True, confidence=0.9) != None:
-         keyboard.press_and_release('Enter')
+         keyboard.press_and_release('F')
          time.sleep(0.5)
-         keyboard.press_and_release('L')
-         time.sleep(0.5)
-         keyboard.press_and_release('O')
-         time.sleep(0.5)
-         keyboard.press_and_release('L')         
-         time.sleep(0.5)
-         keyboard.press_and_release('Enter')
+
 
     if pyautogui.locateOnScreen('Contunue.PNG', region=(773,581,379,304), grayscale=True, confidence=0.6) != None:
          pyautogui.click(952, 717)
